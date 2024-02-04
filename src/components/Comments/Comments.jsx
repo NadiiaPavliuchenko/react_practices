8e5d74d5f7ca1acfcd67f11bff82b3e17aee0ae9
IvanImen <vanyagarret@gmail.com>
2024-02-04 16:37:59 +0200

    add filter

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -3,13 +3,19 @@ import PropTypes from "prop-types";
 import { Comment } from "../Comment/Comment";
 import { Grid } from "../Grid/Grid";
 import { useGetCommentsQuery } from "../../redux/commentApi";
+import { selectFilter } from "../../redux/filterSlice";
+import { useSelector } from "react-redux";
 
 export const Comments = () => {
+  const filter = useSelector(selectFilter);
   const { data: comments } = useGetCommentsQuery();
+  const filteredComments = comments?.filter((comment) =>
+    comment.content.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     comments && (
       <Grid>
-        {comments.map((comment) => (
+        {filteredComments.map((comment) => (
           <Comment key={comment.id} {...comment} />
         ))}
       </Grid>
